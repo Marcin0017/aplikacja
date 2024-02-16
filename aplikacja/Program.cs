0f040f22ad3d3f6bdaffa15946748ebceac9006f
Marcin0017 <Marcin.Madon1@o2.pl>
2024-02-16 21:40:15 +0100

    Dodaj pliki projektów.

---
+++ aplikacja/Program.cs
@@ -0,0 +1,19 @@
+﻿string name = "Ewa";
+string sex = "Mężczyzna";
+var age = 30;
+
+if (sex == "Kobieta")
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta ponizej 30 lat");
+    }
+    else if (age == 30 && name == "Ewa")
+    {
+        Console.WriteLine("Ewa ma 30 lat");
+    }
+}
+else if (sex == "Mezczzyzna" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
